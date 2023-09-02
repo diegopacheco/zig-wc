50e9c263d762e336f37dce2a205e9e52f029ad34
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-02 01:03:29 -0700

    improve code

--- wc.zig
+++ wc.zig
@@ -1,12 +1,27 @@
 const std = @import("std");
 
-pub fn main() !void{
-    var reader = std.io.getStdIn().reader();
-    var count: u32 = 0;
+fn wordCount(reader:anytype) !i64 {
+    var count: i64 = 0;
     var buffer: [1024]u8 = undefined;
     while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
         _ = line;
         count += 1;
     }
-    std.debug.print("{d}", .{count});
+    return count;
+}
+
+pub fn main() !void{
+    var reader = std.io.getStdIn().reader();
+    const total:i64 = try wordCount(reader);
+    std.debug.print("{d}", .{total});
+}
+
+test "test count"{
+    var writer = std.io.getStdIn().writer();
+    writer.write("test 1\n");
+    writer.write("test 2\n");
+    
+    var reader = std.io.getStdIn().reader();
+    const total:i64 = try wordCount(reader);
+    std.testing.expectEqual(2, total);
 }
