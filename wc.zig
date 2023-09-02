7c79093d5d0c9c66aa6d3abd8f93edae7dd80c5e
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-02 01:28:39 -0700

    improve the code

--- wc.zig
+++ wc.zig
@@ -2,11 +2,13 @@ const std = @import("std");
 
 pub const WordCounter = struct {
 
-    pub fn count(reader:anytype) !i64 {
+    reader: std.io.Reader,
+
+    pub fn count(self:WordCounter) !i64 {
         var total: i64 = 0;
         var buffer: [1024]u8 = undefined;
 
-        var buf = std.io.bufferedReader(reader);
+        var buf = std.io.bufferedReader(self.reader);
         var r = buf.reader();
         
         while (try r.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
@@ -19,7 +21,8 @@ pub const WordCounter = struct {
 
 pub fn main() !void{
     var reader = std.io.getStdIn().reader();
-    const total:i64 = try WordCounter.count(reader);
+    const wc = WordCounter{.reader = reader};
+    const total:i64 = try wc.count(reader);
     std.debug.print("{d}", .{total});
 }
 
@@ -29,7 +32,8 @@ test "test count"{
     var reader = file.reader();
     
     const expected:i64 = 5;
-    const result:i64 = try WordCounter.count(reader);
+    const wc = WordCounter{.reader = reader};
+    const result:i64 = try wc.count(reader);
     try std.testing.expectEqual(expected, result);
 }
 
@@ -39,6 +43,7 @@ test "test empty"{
     var reader = file.reader();
     
     const expected:i64 = 0;
-    const result:i64 = try WordCounter.count(reader);
+    const wc = WordCounter{.reader = reader};
+    const result:i64 = try wc.count();
     try std.testing.expectEqual(expected, result);
 }
