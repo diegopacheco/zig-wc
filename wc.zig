edb73756f7bdaa99c5b3ff71f8f4c8ab25ba1071
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-02 00:33:55 -0700

    progress

--- wc.zig
+++ wc.zig
@@ -1,5 +1,16 @@
 const std = @import("std");
 
+pub fn main() !void{
+    var reader = std.io.getStdIn().reader();
+    var count: u32 = 0;
+    var buf: [1000]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        _ = line;
+        count += 1;
+    }
+    std.debug.print("lines {}", .{count});
+}
+
 test "happyPath" {
     
 }
