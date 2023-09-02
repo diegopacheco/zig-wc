fc78569908465d5e4b910d1de686f64129b15924
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-02 00:42:28 -0700

    working

--- wc.zig
+++ wc.zig
@@ -3,9 +3,8 @@ const std = @import("std");
 pub fn main() !void{
     var reader = std.io.getStdIn().reader();
     var count: u32 = 0;
-    var buf: [1000]u8 = undefined;
-    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        _ = line;
+    var buffer: [1024]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) {
         count += 1;
     }
     std.debug.print("{d}", .{count});
