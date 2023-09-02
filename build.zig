299bcfcd902d2613751329d698df2f89d1a0f758
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-02 00:37:51 -0700

    improve build

--- build.zig
+++ build.zig
@@ -1,22 +1,32 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("wc", .{
-        .source_file = .{
-            .path = "wc.zig",
-        },
-    });
-
-    const tests = b.addTest(.{
+    const exe = b.addExecutable(.{
+        .name = "wc",
         .root_source_file = .{ .path = "wc.zig" },
         .target = target,
         .optimize = optimize,
     });
-    const run_tests = b.addRunArtifact(tests);
 
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_tests.step);
+    b.installArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "wc.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
 }
